da4c9a1a2b526c0b41e85276c28a878fc24f15b4
Nihal mohamed <73734438+Nihalmohamed-10@users.noreply.github.com>
2025-05-22 16:22:22 +0530

    finished

--- src/container/CuratedEssential.jsx
+++ src/container/CuratedEssential.jsx
@@ -2,24 +2,42 @@ import React from "react";
 import clsx from "clsx";
 import CurvyDivs from "../components/CurvyDivs";
 import aboutData from "../../src/constants/index";
+import { motion } from "framer-motion";
 
 function CuratedEssential() {
+  const slideUp = {
+    hidden: { opacity: 0, y: 60 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
     <div
       id="curated-essential"
-      className="bg-[#FDE6F6] font-dm-sans pt-[60px] md:pt-[100px] pb-[60px] md:pb-[60px] px-4 md:w-full"
+      className="bg-[#FDE6F6]  pt-[60px] md:pt-[100px] pb-[60px] md:pb-[60px] px-4 md:w-full"
     >
       <div className="text-center mx-auto max-w-2xl px-2">
-        <p className="text-3xl sm:text-4xl sm:w-[77%] sm:mx-auto md:text-[60px] leading-snug md:leading-[80px] md:w-full font-medium">
+        {/* <p className="text-3xl sm:text-4xl sm:w-[77%] sm:mx-auto md:text-[60px] leading-snug md:leading-[80px] md:w-full font-medium">
           Curated Essentials for Her Comfort
-        </p>
+        </p> */}
+        <motion.p
+          variants={slideUp}
+          initial="hidden"
+          animate="visible"
+          className="font-dm-serif text-3xl sm:text-4xl sm:w-[77%] sm:mx-auto md:text-[60px] leading-snug md:leading-[80px] md:w-full font-medium"
+        >
+          Curated Essentials for Her Comfort
+        </motion.p>
       </div>
       <div className="flex flex-col items-center justify-evenly mt-12 md:mt-[85px] gap-10 md:flex-row md:flex-wrap xl:pl-[120px] xl:pr-[120px] xl:mt-0">
         {aboutData.map((item) => (
-          <div
-            key={item.id}
-            className={clsx(item.id % 2 === 0 && "2xl:pt-35")}
-          >
+          <div key={item.id} className={clsx(item.id % 2 === 0 && "2xl:pt-35")}>
             <CurvyDivs
               title={item.title}
               img={item.img}
@@ -35,7 +53,6 @@ function CuratedEssential() {
 }
 
 export default CuratedEssential;
-
 
 // import React from "react";
 // import clsx from "clsx";
@@ -56,7 +73,7 @@ export default CuratedEssential;
 //             key={item.id}
 //             className={clsx( item.id % 2===0 && "2xl:pt-35"
 //               // "xl:pt-35": item.id % 2 === 0,
-              
+
 //             )}
 //           >
 //             <CurvyDivs title={item.title} img={item.img} isEven={item.id % 2 === 0} />
--- src/container/SpotLight.jsx
+++ src/container/SpotLight.jsx
@@ -40,7 +40,7 @@ const Spotlight = () => {
           variants={slideUp}
           initial="hidden"
           animate="visible"
-          className="font-dm-sans text-[52px] sm:text-4xl sm:text-start sm:w-[77%] font-[400] leading-tight mb-6 lg:w-[47%] xl:text-[50px] xl:w-[404px] 2xl:text-[72px] 2xl:w-[583px] 2xl:font-normaly"
+          className="font-dm-serif text-[52px] sm:text-4xl sm:text-start sm:w-[77%] font-[400] leading-tight mb-6 lg:w-[47%] xl:text-[50px] xl:w-[404px] 2xl:text-[72px] 2xl:w-[583px] 2xl:font-normaly"
         >
           From Periods to Pampering—We’ve Got Her Covered
         </motion.h1>
