ea821bb83f9b6597fff45c9ee1b677ab6737dfd7
Nihal mohamed <73734438+Nihalmohamed-10@users.noreply.github.com>
2025-07-06 15:53:34 +0530

    whatsapContent

--- src/components/WhatsAppChaButton.jsx
+++ src/components/WhatsAppChaButton.jsx
@@ -1,11 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const WhatsAppChatButton = () => {
   return (
     <a
       href="https://wa.me/918606050922?text=Hi%2C%20I%20would%20like%20to%20know%20details%20of%20your%20Product%20%F0%9F%98%8A"
-
       target="_blank"
       rel="noreferrer noopener"
       className="fixed bottom-10 right-10 z-50 inline-flex items-center justify-center w-14 h-14 rounded-full bg-[#25d366]"
@@ -19,7 +18,3 @@ const WhatsAppChatButton = () => {
 };
 
 export default WhatsAppChatButton;
-
-
-
-
